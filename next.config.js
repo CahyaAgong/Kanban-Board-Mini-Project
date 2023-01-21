c29b9ce5212721d3486c05273c3d3272fec332a2
cahya_agung <tjahja_lol@yahoo.com>
2023-01-21 22:05:50 +0700

    update relative path to v1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/v1',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/v1',
+        basePath: false,
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
